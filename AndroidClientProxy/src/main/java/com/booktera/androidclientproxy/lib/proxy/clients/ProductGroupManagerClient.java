aab88ce42250aa56a736070c8261df8769fedb3f
norbert.virth <norbert.virth@gmail.com>
2015-02-27 11:47:38 +0100

    Implemented ProductGroupManagerClient.getFullDetailed(...)

--- AndroidClientProxy/src/main/java/com/booktera/androidclientproxy/lib/proxy/clients/ProductGroupManagerClient.java
+++ AndroidClientProxy/src/main/java/com/booktera/androidclientproxy/lib/proxy/clients/ProductGroupManagerClient.java
@@ -1,6 +1,7 @@
 package com.booktera.androidclientproxy.lib.proxy.clients;
 
 import com.booktera.androidclientproxy.lib.models.ProductModels.BookBlockPLVM;
+import com.booktera.androidclientproxy.lib.models.ProductModels.BookRowPLVM;
 import com.booktera.androidclientproxy.lib.models.ProductModels.InCategoryPLVM;
 import com.booktera.androidclientproxy.lib.proxy.Request;
 import com.booktera.androidclientproxy.lib.proxy.base.RestServiceClientBase;
@@ -28,6 +29,20 @@ public class ProductGroupManagerClient extends RestServiceClientBase implements 
         request.requestData.put("pageNumber", pageNumber);
         request.requestData.put("productsPerPage", productsPerPage);
         request.requestData.put("needCategory", needCategory);
+
+        sendRequest(request);
+    }
+
+    @Override
+    public void getFullDetailed(String friendlyUrl, int pageNumber, int productsPerPage, Action_1<BookRowPLVM> todoWithResponse, Action_1<HttpResponse> todoIfResponseFailed)
+    {
+        Request<BookRowPLVM> request = new Request<>(BookRowPLVM.class, 3);
+        request.requestUrl = baseAddress + "/GetFullDetailed";
+        request.todoWithResponse = todoWithResponse;
+        request.todoIfResponseFailed = todoIfResponseFailed;
+        request.requestData.put("friendlyUrl", friendlyUrl);
+        request.requestData.put("pageNumber", pageNumber);
+        request.requestData.put("productsPerPage", productsPerPage);
 
         sendRequest(request);
     }
--- AndroidClientProxy/src/main/java/com/booktera/androidclientproxy/lib/proxy/clients/interfaces/IProductGroupManagerClient.java
+++ AndroidClientProxy/src/main/java/com/booktera/androidclientproxy/lib/proxy/clients/interfaces/IProductGroupManagerClient.java
@@ -1,6 +1,7 @@
 package com.booktera.androidclientproxy.lib.proxy.clients.interfaces;
 
 import com.booktera.androidclientproxy.lib.models.ProductModels.BookBlockPLVM;
+import com.booktera.androidclientproxy.lib.models.ProductModels.BookRowPLVM;
 import com.booktera.androidclientproxy.lib.models.ProductModels.InCategoryPLVM;
 import com.booktera.androidclientproxy.lib.utils.Action_1;
 import org.apache.http.HttpResponse;
@@ -11,4 +12,5 @@ import org.apache.http.HttpResponse;
 public interface IProductGroupManagerClient
 {
     void search(String searchText, int pageNumber, int productsPerPage, boolean needCategory, Action_1<BookBlockPLVM> todoWithResponse, Action_1<HttpResponse> todoIfResponseFailed);
+    void getFullDetailed(String friendlyUrl, int pageNumber, int productsPerPage, Action_1<BookRowPLVM> todoWithResponse, Action_1<HttpResponse> todoIfResponseFailed);
 }
